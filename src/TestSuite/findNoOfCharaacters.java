4829acc2a8a507a298e85dc5b3569d82c664b6ec
rohit ranjan <rohitranjan@johndeere.com>
2019-10-01 17:18:03 +0530

    Add AutoDemosite

---
+++ src/TestSuite/findNoOfCharaacters.java
@@ -0,0 +1,75 @@
+package TestSuite;
+
+public class findNoOfCharaacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		String str ="Note:Deere Admin/Professional Employee Class Positions require but Deere Production/Maintenance "
+//				+ "Employee Class positions don't require Job Match Justification."
+//				+ "so this field will not be visible.Document the most important,specific duties of the "
+//				+ "position.";
+//		
+//		System.out.println(str.length());
+//		
+//		System.out.println(str);
+		
+//		**********************************************************************	
+		
+//		String str1 ="This attribute controls whether the target FTE is checked when an employee is "
+//				+ "assigned to this position.In addition,the attribute triggers the stable headcount "
+//				+ "processing when an employee is assigned to a new position during Position Transfer or "
+//				+ "Position";
+//		// Reclassification.
+//		
+//		System.out.println(str1.length());		
+//		System.out.println(str1.length()-255);
+//		
+//		System.out.println(str1);
+		
+//      ******************************************************************************		
+		
+//		String str2 ="This editable field is for the std hrs on job info and pos,can be "
+//				+ "included in the sync between position and emp.Here,"
+//				+ "emp's FTE value gets calc on std hrs inherited from the "
+//			    + "emp's assigned pos.The Std Hrs comes from table on Job and "
+//			    + "propogates to the pos.";
+//		
+//		System.out.println(str2.length());		
+//		System.out.println(str2.length()-255);
+//		
+//		System.out.println(str2);
+
+		
+//		************************************************************************************
+//		String str4 ="KNOWN ISSUE-Pay Range is not autopopulated for 'Copy' Position functionality."
+//				+ "If the Pay Range is not auto-populated,tester must perform work around in order "
+//				+ "to utilize this position for transaction testing."
+//				+ "Work around:Click on the Position,";
+		String str4 = "ApproveTo Do(requires pre-req to set up a To-Do)\r\n" + 
+				"Approvals:\r\n" + 
+				"• SpotBonus req\r\n" + 
+				"• Salary IncreaseReq\r\n" + 
+				"• Job Requisition approvals\r\n" + 
+				"• JobOffer approvals\r\n" + 
+				"• Employee Information ChangeReq \r\n" + 
+				"• Learning Course approvals\r\n" + 
+				"• TimeSheet approvals-Not Scope for Jan";
+		
+//		String str4 ="NOTE: Copies all fields so only utilize this functionality when nothing changes on "
+//				+ "the position.Deere Admin/Prof emp Class Pos req a "
+//				+ "Workforce Planning ,Job Match Justification-So need to go in and "
+//				+ "update these fields by going into the pos via Manage Data";
+//				+ "NOTE:Deere Production/Maintenance Employee Class positions and Job Match Justification"
+//				+ " don't require a Workforce "
+//				+ "Planning so not visible";
+		
+		
+		System.out.println(str4.length());		
+		System.out.println(str4.length()-255);	
+		System.out.println(str4);
+		
+		
+	}
+
+}
